5904cb758f55e354af972fc55b48f5ec3ed19973
Soulie <jack.davenport@ymail.com>
2019-04-12 13:54:41 +0100

    Cleaned up some code.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -59,14 +59,14 @@ public class GameController : MonoBehaviour
         // Populate the planetstats dictionary
         planetstats = new Dictionary<string, PlanetStats>();
 
-        PlanetStats mercuryStats = new PlanetStats("Mercury", "Mercury has very little atmosphere.", 4878, 167, 3.7f, 0, 57.9d);
-        PlanetStats venusStats = new PlanetStats("Venus", "Venus spins backwards compared to other planets.", 12100, 464, 8.9f, 0, 108.2d);
-        PlanetStats earthStats = new PlanetStats("Earth", "Our home planet.", 12742, 15, 9.8f, 1, 149.6d);
-        PlanetStats marsStats = new PlanetStats("Mars", "Mars is the closest planet to our home.", 6779, -65, 3.7f, 2, 227.9d);
-        PlanetStats jupiterStats = new PlanetStats("Jupiter", "Jupiter is the biggest planet in our solar system.", 142981, -110, 23.1f, 79, 778.3d);
-        PlanetStats saturnStats = new PlanetStats("Saturn", "Saturn has winds that reach up to 500 meters per second.", 120536, -140, 9.0f, 62, 1427d);
-        PlanetStats uranusStats = new PlanetStats("Uranus", "Uranus is a gas giant that spins backwards like Venus.", 51118, -195, 8.7f, 27, 2871d);
-        PlanetStats neptuneStats = new PlanetStats("Neptune", "Neptune has the second largest gravity - second only to Jupiter.", 49500, -200, 11f, 14, 4497d);
+        PlanetStats mercuryStats = new PlanetStats("Mercury", "Mercury has very little atmosphere.", 4879, 167, 3.7f, 0, 57.9d);
+        PlanetStats venusStats = new PlanetStats("Venus", "Venus spins backwards compared to other planets.", 12104, 464, 8.9f, 0, 108.2d);
+        PlanetStats earthStats = new PlanetStats("Earth", "Our home planet.", 12756, 15, 9.8f, 1, 149.6d);
+        PlanetStats marsStats = new PlanetStats("Mars", "Mars is the closest planet to our home.", 6792, -65, 3.7f, 2, 227.9d);
+        PlanetStats jupiterStats = new PlanetStats("Jupiter", "Jupiter is the biggest planet in our solar system.", 142984, -110, 23.1f, 79, 778.6d);
+        PlanetStats saturnStats = new PlanetStats("Saturn", "Saturn has winds that reach up to 500 meters per second.", 120536, -140, 9.0f, 62, 1433d);
+        PlanetStats uranusStats = new PlanetStats("Uranus", "Uranus is a gas giant that spins backwards like Venus.", 51118, -195, 8.7f, 27, 2872d);
+        PlanetStats neptuneStats = new PlanetStats("Neptune", "Neptune has the second largest gravity - second only to Jupiter.", 49528, -200, 11f, 14, 4495d);
 
         planetstats.Add("Mercury", mercuryStats);
         planetstats.Add("Venus", venusStats);
